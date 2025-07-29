35ce7025ea3b5241271fd5bce6c6d25548e7cf19
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-07-30 01:21:09 +0900

    otpSender commit push

--- src/bankService/infra/ConsoleOtpSender.java
+++ src/bankService/infra/ConsoleOtpSender.java
@@ -14,10 +14,7 @@ public class ConsoleOtpSender implements OtpSender {
     /**
      * 실제 전송 대신 System.out 으로 표시하여 사용자가 OTP 를 볼 수 있게 함
      */
-    @Override
     public void send(String dest, String code) {
-        System.out.printf("[DEBUG] OTP to %s : %s%n", dest, code);          // 학습용: 콘솔 출력으로 전송 대체
-
 
     }
 }
