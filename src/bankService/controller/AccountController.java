0330ab2ebc887d502aecab256b9027432ecff9d1
LEE-GYEO-RE <rufp9891@gmail.com>
2025-08-02 23:10:35 +0900

    이겨레 / dao 하나로 통일

--- src/bankService/controller/AccountController.java
+++ src/bankService/controller/AccountController.java
@@ -1,10 +1,8 @@
 package bankService.controller;
 
 import bankService.model.dao.AccountDao;
-import bankService.model.dao.ExistenceDao;
 
 public class AccountController {
-    // AccountDao , ExistenceDao  불러올  controller
 
     // 싱글톤 생성
     private AccountController(){}
@@ -13,12 +11,18 @@ public class AccountController {
         return instance;
     }
 
-    // AccountDao , ExistenceDao 싱글톤 가져오기
+    // AccountDao 싱글톤 가져오기
     public AccountDao accountDao = AccountDao.getInstance();
-    public ExistenceDao existenceDao = ExistenceDao.getInstance();
 
 
-    //
+
+    // 입금 메소드
+
+    // 출금 메소드
+
+    // 이체 메소드
+
+
 
 
 }
